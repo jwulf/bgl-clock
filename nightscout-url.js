209977765c578b6bc16408b9e5b34f8c2cff53a2
Joshua Wulf <sitapati@worldsankirtan.net>
2018-12-30 23:10:56 +1000

    Add note about cors in Nightscout

--- nightscout-url.js
+++ nightscout-url.js
@@ -1,1 +1,5 @@
+/*
+    Add cors to the Nightscout plugins.
+    See: https://github.com/nightscout/cgm-remote-monitor#cors-cors
+*/
 var nightscoutUrl = 'https://cgm.prahlads.space/api/v1/entries/current.json'
